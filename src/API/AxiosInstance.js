5c7d5a40e4d744451067b9fb60ee1b10d2a61d10
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-06-03 13:32:38 +0700

    v3.4

--- src/API/AxiosInstance.js
+++ src/API/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AxiosInstance = axios.create({
-  baseURL: "https://api-planta.onrender.com/",
+  baseURL: "https://api-planta-sa.onrender.com/",
   responseType: "json",
 });
 
